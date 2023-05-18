1b2f1ec4aaf1df4b2b22798b83a95a2e66338561
Yashveer Bika <yashveerbika@gmail.com>
2023-05-18 16:01:55 -0500

    implement search bar on filterable searchable exercise table

--- frontend/src/components/FilterableSearchableExerciseTable.tsx
+++ frontend/src/components/FilterableSearchableExerciseTable.tsx
@@ -12,126 +12,154 @@ interface Exercise {
     equipment: string
 }
 
+const exercises = [
+    {
+        id: 1,
+        name: "21s Biceps Curls",
+        imgPath: "",
+        muscle: "Biceps",
+        equipment: "Dumbbell"
+    },
+    {
+        id: 2,
+        name: "Cable Pressdown",
+        imgPath: "",
+        muscle: "Triceps",
+        equipment: "Cable"
+    },
+    {
+        id: 3,
+        name: "21s Biceps Curls",
+        imgPath: "",
+        muscle: "Biceps",
+        equipment: "Dumbbell"
+    },
+    {
+        id: 4,
+        name: "Cable Pressdown",
+        imgPath: "",
+        muscle: "Triceps",
+        equipment: "Cable"
+    },
+    {
+        id: 5,
+        name: "21s Biceps Curls",
+        imgPath: "",
+        muscle: "Biceps",
+        equipment: "Dumbbell"
+    },
+    {
+        id: 6,
+        name: "Cable Pressdown",
+        imgPath: "",
+        muscle: "Triceps",
+        equipment: "Cable"
+    },
+    {
+        id: 7,
+        name: "21s Biceps Curls",
+        imgPath: "",
+        muscle: "Biceps",
+        equipment: "Dumbbell"
+    },
+    {
+        id: 8,
+        name: "Cable Pressdown",
+        imgPath: "",
+        muscle: "Triceps",
+        equipment: "Cable"
+    },
+    {
+        id: 9,
+        name: "21s Biceps Curls",
+        imgPath: "",
+        muscle: "Biceps",
+        equipment: "Dumbbell"
+    },
+    {
+        id: 10,
+        name: "Cable Pressdown",
+        imgPath: "",
+        muscle: "Triceps",
+        equipment: "Cable"
+    },
+    {
+        id: 11,
+        name: "21s Biceps Curls",
+        imgPath: "",
+        muscle: "Biceps",
+        equipment: "Dumbbell"
+    },
+    {
+        id: 12,
+        name: "Cable Pressdown",
+        imgPath: "",
+        muscle: "Triceps",
+        equipment: "Cable"
+    },
+    {
+        id: 13,
+        name: "21s Biceps Curls",
+        imgPath: "",
+        muscle: "Biceps",
+        equipment: "Dumbbell"
+    },
+    {
+        id: 14,
+        name: "Cable Pressdown",
+        imgPath: "",
+        muscle: "Triceps",
+        equipment: "Cable"
+    },
+    {
+        id: 15,
+        name: "21s Biceps Curls",
+        imgPath: "",
+        muscle: "Biceps",
+        equipment: "Dumbbell"
+    },
+    {
+        id: 16,
+        name: "Cable Pressdown",
+        imgPath: "",
+        muscle: "Triceps",
+        equipment: "Cable"
+    },
+]
+
 export default function FilterableSearchableExerciseTable() {
 
-    // const [exercises, setExercises] = useState<Exercise[]>([]);
-    const exercises = [
-        {
-            id: 1,
-            name: "21s Biceps Curls",
-            imgPath: "",
-            muscle: "Biceps",
-            equipment: "Dumbbell"
-        },
-        {
-            id: 2,
-            name: "Cable Pressdown",
-            imgPath: "",
-            muscle: "Triceps",
-            equipment: "Cable"
-        },
-        {
-            id: 3,
-            name: "21s Biceps Curls",
-            imgPath: "",
-            muscle: "Biceps",
-            equipment: "Dumbbell"
-        },
-        {
-            id: 4,
-            name: "Cable Pressdown",
-            imgPath: "",
-            muscle: "Triceps",
-            equipment: "Cable"
-        },
-        {
-            id: 5,
-            name: "21s Biceps Curls",
-            imgPath: "",
-            muscle: "Biceps",
-            equipment: "Dumbbell"
-        },
-        {
-            id: 6,
-            name: "Cable Pressdown",
-            imgPath: "",
-            muscle: "Triceps",
-            equipment: "Cable"
-        },
-        {
-            id: 7,
-            name: "21s Biceps Curls",
-            imgPath: "",
-            muscle: "Biceps",
-            equipment: "Dumbbell"
-        },
-        {
-            id: 8,
-            name: "Cable Pressdown",
-            imgPath: "",
-            muscle: "Triceps",
-            equipment: "Cable"
-        },
-        {
-            id: 9,
-            name: "21s Biceps Curls",
-            imgPath: "",
-            muscle: "Biceps",
-            equipment: "Dumbbell"
-        },
-        {
-            id: 10,
-            name: "Cable Pressdown",
-            imgPath: "",
-            muscle: "Triceps",
-            equipment: "Cable"
-        },
-        {
-            id: 11,
-            name: "21s Biceps Curls",
-            imgPath: "",
-            muscle: "Biceps",
-            equipment: "Dumbbell"
-        },
-        {
-            id: 12,
-            name: "Cable Pressdown",
-            imgPath: "",
-            muscle: "Triceps",
-            equipment: "Cable"
-        },
-        {
-            id: 13,
-            name: "21s Biceps Curls",
-            imgPath: "",
-            muscle: "Biceps",
-            equipment: "Dumbbell"
-        },
-        {
-            id: 14,
-            name: "Cable Pressdown",
-            imgPath: "",
-            muscle: "Triceps",
-            equipment: "Cable"
-        },
-        {
-            id: 15,
-            name: "21s Biceps Curls",
-            imgPath: "",
-            muscle: "Biceps",
-            equipment: "Dumbbell"
-        },
-        {
-            id: 16,
-            name: "Cable Pressdown",
-            imgPath: "",
-            muscle: "Triceps",
-            equipment: "Cable"
-        },
-    ]
+    const [activeExercises, setActiveExercises] = useState<Exercise[]>(exercises);
 
-    function handleSearch() {
-        // TODO: dynamically filter the exercise blocks that can render
+
+    function handleSearch(query : string) {
+        // TODO: dynamically filter the activeExercise blocks that can render
+
+        function searchPredicate(exercise : Exercise) {
+            // if ( exercise.name.includes(query) )  {
+            //     return true;
+            // }
+            // else if (exercise.muscle.includes(query) ) {
+            //     return true;
+            // }
+            // else if ( exercise.equipment.includes(query) ) {
+            //     return true;
+            // } else {
+            //     return false;
+            // }
+            return exercise.name.toLowerCase().includes(query.toLowerCase()) || 
+                exercise.muscle.toLowerCase().includes(query.toLowerCase()) || 
+                exercise.equipment.toLowerCase().includes(query.toLowerCase());
+        }
+
+        const tempActiveExercises = exercises.filter(searchPredicate);
+        console.log(`Query: ${query}`);
+        if (query = "") {
+            setActiveExercises(exercises);
+        } else {
+            setActiveExercises(tempActiveExercises);
+        }
+        console.log(activeExercises);
     }
 
     return (
@@ -139,14 +167,19 @@ export default function FilterableSearchableExerciseTable() {
             <Box className="filterable-searchable-exercise-table-inner-container">
 
                 <Box> <p>Filters</p> </Box> 
+                
                 <Box> <p>Equipment Filter</p> </Box> 
+                
                 <Box> <p>Muscle Filter</p> </Box> 
+                
                 <Box className="header-container"> <p>Library</p> <Button>+ Create Exercise</Button> </Box> 
+                
                 <Box> 
                         <SearchBar onSearch={handleSearch}></SearchBar> 
                     </Box>
+
                 <Box className="exercise-table-container">
-                    {exercises.map(
+                    {activeExercises.map(
                         (exercise, index) => 
                         <ExerciseRow key={index} exercise={exercise} >  </ExerciseRow>
                         )
--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Input, Button, Flex } from '@chakra-ui/react';
 
 const SearchBar = ({ onSearch } : any) => {
   const [searchQuery, setSearchQuery] = useState('');
 
+  useEffect(() => {
+    // Trigger search logic whenever searchQuery changes
+    onSearch(searchQuery);
+  }, [searchQuery]);
+
   const handleChange = (event : any) => {
     setSearchQuery(event.target.value);
-    onSearch(searchQuery);
   };
 
   return (
