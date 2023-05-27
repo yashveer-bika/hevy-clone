b4fad4bbcab0c65ed6f06eb845372d6378e56ceb
Yashveer Bika <yashveerbika@gmail.com>
2023-05-27 11:46:24 -0500

    link to my Github on Subscription page

--- frontend/src/components/settings/Subscription.tsx
+++ frontend/src/components/settings/Subscription.tsx
@@ -1,10 +1,27 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Text, Link } from "@chakra-ui/react";
 
+import "../../styles/settings/Subscription.css"
 
 export default function Account() {
+    // TODO: make a contact page to contact page, plug my Github, resume, email, etc.
+    // TODO: set up a mini Payment system to pay me
+    
     return (
         <Box>
-            
+            <Box>
+                <Text>
+                    Interesting in viewing the{' '}
+                        <Link color='teal.500' href='https://github.com/yashveer-bika/hevy-clone'>
+                            source code
+                        </Link>
+                </Text>
+                <Text>
+                    Interesting in more other work by Yashveer Bika{' '}
+                        <Link color='teal.500' href='https://github.com/yashveer-bika/'>
+                            Github
+                        </Link>
+                </Text>
+            </Box>
         </Box>
     );
 }
