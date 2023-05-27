63cf867a3ac5cdec91f68323bb919fcdc5cdf61a
Yashveer Bika <yashveerbika@gmail.com>
2023-05-27 13:54:48 -0500

    theme wireframe

--- frontend/src/components/settings/Theme.tsx
+++ frontend/src/components/settings/Theme.tsx
@@ -1,10 +1,14 @@
 import { Box } from "@chakra-ui/react";
+import DropdownFilter from "../DropdownFilter";
 
 
 export default function Account() {
     return (
-        <Box>
-            
+        <Box className="account-container">
+            <Box className="unit-container">
+                <p className="title"> Weight Unit </p>
+                <DropdownFilter options={["lbs", "kg"]}></DropdownFilter>
+            </Box>
         </Box>
     );
 }
