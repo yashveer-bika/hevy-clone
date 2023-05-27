28c029bae2aabd644ae9d1df9d6e6975f55a060f
Yashveer Bika <yashveerbika@gmail.com>
2023-05-27 14:02:27 -0500

    wireframe export page

--- frontend/src/components/settings/Export.tsx
+++ frontend/src/components/settings/Export.tsx
@@ -1,9 +1,19 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Button, Divider } from "@chakra-ui/react";
+import DropdownFilter from "../DropdownFilter";
 
 
-export default function Account() {
+export default function Export() {
+
+    function handleExportData() {
+
+    }
+
     return (
-        <Box>
+        <Box className="account-right-side-container">
+            <p> Export Workout Data </p>
+            <p> Export your entire workout history to a CSV file. Exported workouts data can not be imported back into Hevy </p>
+            <Divider></Divider>
+            <Button onClick={handleExportData}>Export Workout Data</Button>
             
         </Box>
     );
